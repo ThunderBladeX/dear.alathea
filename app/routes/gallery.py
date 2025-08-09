771454b8789b5a254392d014e8238f7d8f175032
ThunderBladeX <magicalpurplehearth@gmail.com>
2025-08-10 00:08:12 +0700

    Update gallery.py

--- app/routes/gallery.py
+++ app/routes/gallery.py
@@ -2,7 +2,13 @@ from flask import Blueprint, render_template, request, redirect, url_for, sessio
 from app.database import execute_query
 from app.services.comment_service import get_comments_with_replies
 
-gallery_bp = Blueprint('gallery', __name__, url_prefix='/gallery')
+gallery_bp = Blueprint(
+    'gallery',
+    __name__,
+    template_folder='../templates',
+    static_folder='../static',
+    url_prefix='/gallery'
+)
 
 @gallery_bp.route('/')
 def index():
@@ -22,8 +28,7 @@ def image_detail(image_id):
     
     if not image:
         return redirect(url_for('gallery.index'))
-    
-    # Get comments using optimized service
+
     comments = get_comments_with_replies('gallery', image_id)
     
     return render_template('gallery_image.html', image=image, comments=comments)
